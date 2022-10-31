ff2ca0b30923805809653b8dedff732bd8d1547f
GarrettA01 <115960428+GarrettA01@users.noreply.github.com>
2022-10-30 20:29:50 -0600

    Added prompt and if statement for password length, as well as confirmations and an if statement for each character set

--- script.js
+++ script.js
@@ -4,23 +4,38 @@ var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
 var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var specialArr = ['!', '@', '#', '$', '*', '%']
 
-function getUsersPasswordOptions() {
+function generatePassword() {
 // - Create prompt for password length - Create var that changed selection 
 // into number type
 var passwordLength = parseInt(prompt("Please select a apssword length between 8 and 128 characters."), 10)
 
-console.log(passwordLength)
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Please make sure selection is between 8 and 128 characters!")
+  return "ERROR"
+} else {
+
+
 // - Create if/else statement for when length is either < 8 or > 128
 
 // - Create confirmations for each charcter set (A, a, 0-9, !)
-var hasNumbers = confirm("click ok to include numbers in your password")
+var hasLower = confirm("Would you like lowercase letters in your password? (a)")
+var hasUpper = confirm("Would you like uppercase letters in your password? (A)")
+var hasNumbers = confirm("Would you like numbers in your password? (0-9)")
+var hasSpecial = confirm("Would you like special characters in your password? (!, @, etc.)")
+
+if (!hasLower && !hasUpper && !hasNumbers && !hasSpecial) {
+  alert ("You must select at least one character type!")
+  return "ERROR"
+} else {
 // - Create if/else statement for when no charcters are selected
 
 // - Create empty variable for selected charcter sets to go in (object)
 var usersPasswordChoices = {
   passwordLength: passwordLength,
-  hasNumbers: hasNumbers,
-  // add other fields
+  hasLower: lowerCaseArr,
+  hasUpper: upperCaseArr,
+  hasNumbers: numbersArr,
+  hasSpecial: specialArr
 }
 return usersPasswordChoices;
 }
@@ -42,6 +57,8 @@ function generateUserPassword() {
 // - Send password to html
 }
 
+}
+}
 
 
 // Assignment Code
