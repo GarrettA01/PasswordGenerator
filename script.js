11712b17096aee54ff6996ad39dd5501af8b079a
GarrettA01 <115960428+GarrettA01@users.noreply.github.com>
2022-10-30 20:57:58 -0600

    Added array for each chosen array set to go into

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// - Create variable arrays for each character set (A, a, 0-9, !)
+// - Variable arrays for each character set (A, a, 0-9, !)
 var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var specialArr = ['!', '@', '#', '$', '*', '%']
 
 function generatePassword() {
-// - Create prompt for password length - Create var that changed selection 
-// into number type
-var passwordLength = parseInt(prompt("Please select a apssword length between 8 and 128 characters."), 10)
+// Prompt for password length, if statement for if user chooses a number < 8 or > 128
+var passwordLength = parseInt(prompt("Please select a password length between 8 and 128 characters."), 10)
 
 if (passwordLength < 8 || passwordLength > 128) {
   alert("Please make sure selection is between 8 and 128 characters!")
@@ -15,9 +14,9 @@ if (passwordLength < 8 || passwordLength > 128) {
 } else {
 
 
-// - Create if/else statement for when length is either < 8 or > 128
 
-// - Create confirmations for each charcter set (A, a, 0-9, !)
+
+// Confirmations for each character selection, if statement for if none are chosen
 var hasLower = confirm("Would you like lowercase letters in your password? (a)")
 var hasUpper = confirm("Would you like uppercase letters in your password? (A)")
 var hasNumbers = confirm("Would you like numbers in your password? (0-9)")
@@ -27,40 +26,27 @@ if (!hasLower && !hasUpper && !hasNumbers && !hasSpecial) {
   alert ("You must select at least one character type!")
   return "ERROR"
 } else {
-// - Create if/else statement for when no charcters are selected
 
 // - Create empty variable for selected charcter sets to go in (object)
-var usersPasswordChoices = {
-  passwordLength: passwordLength,
-  hasLower: lowerCaseArr,
-  hasUpper: upperCaseArr,
-  hasNumbers: numbersArr,
-  hasSpecial: specialArr
-}
-return usersPasswordChoices;
-}
+var usersPasswordChoices = []
 
-// utility function to get random element from array
-function getRandomValueFromArray(array) {
-  var randomIndex = Math.floor(Math.random() * array.length);
-  var randomElement = array[randomIndex]
-  return randomElement
+if(hasLower){
+  usersPasswordChoices = usersPasswordChoices.concat(lowerCaseArr);
 }
-
-function generateUserPassword() {
-// - Create true/false confirms for each character set, if true, throw array
-// into empty variable 
-// - Create empty variable for final password
-// - Create for loop where i=0, i < password length, i++
-// - Create random array variable within for loop, use math thingy, * wanted characters length, creates randomized characters
-// - Fill final password var with random character array
-// - Send password to html
+if(hasUpper){
+  usersPasswordChoices = usersPasswordChoices.concat(upperCaseArr);
 }
-
+if(hasNumbers){
+  usersPasswordChoices = usersPasswordChoices.concat(numbersArr);
 }
+if(hasSpecial){
+  usersPasswordChoices = usersPasswordChoices.concat(specialArr);
 }
 
 
+}
+}
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
